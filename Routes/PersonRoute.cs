2860b2397f67d157a72e482819b74e4a82c0b1bf
carlosboucas <carlos.rodrigo.cmb@gmail.com>
2024-12-19 11:30:18 -0300

    Criação das classes PersonRequest e PersonRoute

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,4 +1,5 @@
-﻿using Person.Models;
+﻿using Person.Data;
+using Person.Models;
 
 namespace Person.Routes
 {
@@ -6,7 +7,15 @@ namespace Person.Routes
     {
         public static void PersonRoutes(this WebApplication app)
         {
-            app.MapGet(pattern: "person", () => new PersonModel(name:"Rodrigo"));
+            var route = app.MapGroup("person");
+
+            route.MapPost("",
+                async (PersonRequest req, PersonContext context) =>
+                {
+                    var person = new PersonModel(req.name);
+                    await context.AddAsync(person);
+                    await context.SaveChangesAsync();
+                });
         }
     }
 }
